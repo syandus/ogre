2fe2abba1fe5b467971cd96e717362daa906fb38
Pavel Rojtberg <rojtberg@gmail.com>
2020-03-15 00:32:24 +0100

    Main: improve wording of old mesh format warning

--- OgreMain/src/OgreMeshSerializer.cpp
+++ OgreMain/src/OgreMeshSerializer.cpp
@@ -200,9 +200,8 @@ namespace Ogre {
         /*
         if (ver != mVersionData[0]->versionString)
         {
-            LogManager::getSingleton().logWarning( pDest->getName() +
-                " is an older format (" + ver + "); you should upgrade it as soon as possible" +
-                " using the OgreMeshUpgrade tool.");
+            LogManager::getSingleton().logWarning(pDest->getName() + " uses an old format " + ver +
+                                                  "; upgrade with the OgreMeshUpgrader tool");
         }
         */
 
